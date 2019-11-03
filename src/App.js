814623eaa97c1fac0c85f5475727152ec9adea1e
Evan <1773693088@qq.com>
2019-11-03 11:02:52 +0800

    公共部分

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//路由的2种形式： hash(HashRouter) , H5的historyApi(BroswerRouter)是路由的容器，是组件，要包在路由的外面
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Index(){
+  return <p>首页</p>
+}
+
+function Login(){
+  return (<p>登录</p>)
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  // eslint-disable-next-line react/require-render-return
+  render(){
+    return (
+        <Router>
+          <div>
+            {/*exact确切为/时，才会匹配。否则在下面2个路由中，都会显示第一个路由*/}
+            <Route path="/" exact={true} component={Login}></Route>
+            <Route path={'/login'} component={Login}></Route>
+            <Route path={'/index'} component={Index}></Route>
+          </div>
+        </Router>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,55 +1,16 @@
 import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './static/bootstrap/2.3.1/css/bootstrap.min.css';
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Login from "./pages/login/login";
+import indexPage from "./pages/index/indexPage";
 
-class Login extends React.Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-            message: "",
-            rememberMe: true,
-            username: "",
-        };
-    }
-
-    render() {
-        const {message, username, rememberMe} = this.state;
-        return (
-            <Fragment>
-                <div className="header">
-                    <div id="messageBox" className="alert alert-error {empty message ? 'hide' : ''}">
-                        <button data-dismiss="alert" className="close">×</button>
-                        <label id="loginError" className="error">{message}</label>
-                    </div>
-                </div>
-                <h1 className="form-signin-heading">权限管理系统</h1>
-                <div id="loginForm" className="form-sign">
-                    <label className="input-label" htmlFor="username">登录名</label>
-                    <input type="text" id="username" name="username" className="input-block-level required"
-                           value={username} />
-                    <label className="input-label" htmlFor="password">密码</label>
-                    <input type="password" id="password" name="password" className="input-block-level required" />
-                    <input className="btn btn-large btn-primary" type="submit" value="登 录" />&nbsp;&nbsp;
-                    <label htmlFor="rememberMe" title="下次不需要再登录">
-                        <input type="checkbox" id="rememberMe" name="rememberMe" checked="checked"/> 记住我
-                    </label>
-
-                </div>
-                <div className="footer">
-                    Copyright © 2015-2015 <a href="http://www.tanzhouedu.com/" target="_blank">权限管理系统</a> - Powered By
-                    Colin V0.0.1
-                </div>
-            </Fragment>
-        );
-    }
-}
-
-ReactDOM.render(<Login />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<React.Suspense fallback="">
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Login}></Route>
+            <Route exact path="/index/indexPage" component={indexPage}></Route>
+        </Switch>
+    </Router>
+</React.Suspense>, document.getElementById("root"));
